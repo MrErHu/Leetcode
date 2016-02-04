09b3b6ebdb110b5b58adc6abb61b57a8d66c7e50
MrErHu <471023902@qq.com>
2016-02-04 16:33:41 +0800

    iterative
    iterative

--- 144.cpp
+++ 144.cpp
@@ -16,3 +16,26 @@ public:
         }
     }
 };
+/*
+迭代解法
+*/
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*> t_stack;
+        vector<int> result;
+        TreeNode *ptr;
+        if(!root) return result;
+        t_stack.push(root);
+        while(!t_stack.empty()){
+            ptr=t_stack.top();
+            t_stack.pop();
+            result.push_back(ptr->val);
+            if(ptr->right)
+                t_stack.push(ptr->right);
+            if(ptr->left)
+                t_stack.push(ptr->left);
+        }
+        return result;
+    }
+};
