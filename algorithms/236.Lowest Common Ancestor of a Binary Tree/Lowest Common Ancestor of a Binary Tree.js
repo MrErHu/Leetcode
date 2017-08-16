2bac4286b81a428b23cdfa9f038e79718fdc4658
MrErHu <wanglei_cs@163.com>
2017-08-16 09:50:42 +0800

    add 235.Lowest Common Ancestor of a Binary Search Tree

--- algorithms/236.Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.js
+++ algorithms/236.Lowest Common Ancestor of a Binary Tree/Lowest Common Ancestor of a Binary Tree.js
@@ -6,11 +6,27 @@
  * }
  */
 /**
+ * Recursion
  * @param {TreeNode} root
  * @param {TreeNode} p
  * @param {TreeNode} q
  * @return {TreeNode}
  */
-var lowestCommonAncestor = function(root, p, q) {
+var lowestCommonAncestor = function (root, p, q) {
+
+    if (!root || root == p || root == q) {
+        return root;
+    }
+
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
+
+    if(left && right){
+        return root;
+    }else if(left){
+        return left
+    }else {
+        return right;
+    }
 
 };
